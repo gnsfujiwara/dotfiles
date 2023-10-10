c653f854f160521561f48d659e833d428ec7f38b
Gustavo Vinícios de Souza <gnsfujiwara@disroot.org>
2023-10-10 12:35:08 -0300

    Change font

--- tym/.config/tym/config.lua
+++ tym/.config/tym/config.lua
@@ -5,29 +5,29 @@ Author: gnsfujiwara <gnsfujiwara@disroot.org>
 
 --]]
 
-local tym = require 'tym'
+local tym = require("tym")
 
 tym.set_config({
-  font = 'Hack Nerd Font 10',
-  scrollback_length = 0, -- Use tmux
-  autohide = true,
-  cursor_blink_mode = 'off',
-  uri_schemes = 'http https file mailto irc',
+	font = "IosevkaTerm NF 11",
+	scrollback_length = 0, -- Use tmux
+	autohide = true,
+	cursor_blink_mode = "off",
+	uri_schemes = "http https file mailto irc",
 })
 
 tym.set_keymaps({
-  ['<Ctrl><Shift>r'] = function()
-    tym.reload()
-  end,
+	["<Ctrl><Shift>r"] = function()
+		tym.reload()
+	end,
 
-  -- Resize font on the fly
-  ['<Ctrl><Shift>plus'] = function()
-    tym.set('scale', tym.get('scale') + 10)
-  end,
-  ['<Ctrl>minus'] = function()
-    tym.set('scale', tym.get('scale') - 10)
-  end,
-  ['<Ctrl>equal'] = function()
-    tym.set('scale', 100)
-  end,
+	-- Resize font on the fly
+	["<Ctrl><Shift>plus"] = function()
+		tym.set("scale", tym.get("scale") + 10)
+	end,
+	["<Ctrl>minus"] = function()
+		tym.set("scale", tym.get("scale") - 10)
+	end,
+	["<Ctrl>equal"] = function()
+		tym.set("scale", 100)
+	end,
 })
